35f946c7a1d6d1e40502fd5e604c5009bad51fea
yagoazedias <yagoazedias@gmail.com>
2021-02-18 22:28:24 -0300

    Adding board and house classes with simple board drawing

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-import time
-
 import pygame
+from models.board import Board
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode([640, 480])
+    screen = pygame.display.set_mode([700, 700])
     pygame.display.set_caption('Olá mundo')
     screen.fill([0, 0, 0])
     pygame.display.flip()
 
+    board = Board()
+    board.draw(screen)
     running = True
 
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        pygame.display.update()
 
 
 if __name__ == "__main__":
