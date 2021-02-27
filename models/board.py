c9ee76eebd739c36332b1f6bef5667b0fb7c4a75
luizlaljr <luizlaljr@gmail.com>
2021-02-27 10:36:05 -0300

    Board refactored to eigth columns and rows

--- models/board.py
+++ models/board.py
@@ -1,25 +1,26 @@
-from constants.colors import *
 from models.house import House
 
+BOARD_BLACK = 36, 170, 173
+BOARD_WHITE = 148, 233, 235
 
 class Board:
 
     def __init__(self):
         self.turn = 0
         self.winner = None
-        self.houses = [[0 for _ in range(7)] for __ in range(7)]
+        self.houses = [[0 for _ in range(8)] for __ in range(8)]
         self.set_up()
 
     def draw(self, display):
-        for row in range(0, 7):
-            for col in range(0, 7):
+        for row in range(0, 8):
+            for col in range(0, 8):
                 self.houses[row][col].draw(display)
 
     def set_up(self):
-        for row in range(0, 7):
+        for row in range(0, 8):
             color_index = row % 2
-            for col in range(0, 7):
-                color = WHITE if color_index == 0 else BLACK
+            for col in range(0, 8):
+                color = BOARD_WHITE if color_index == 0 else BOARD_BLACK
                 self.houses[row][col] = House(row, col, color)
                 color_index = (color_index + 1) % 2
 
