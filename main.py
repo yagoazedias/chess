ca9c58b7668b05093a06f66ee76b8275d334abb7
Lucas <moreirapdm@gmail.com>
2021-03-22 15:00:29 -0300

    roque e captura en passant implementados

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 from models.board import Board
-from models.ia import Ia
-from constants.types import PAWN, ROOK
+#from models.ia import Ia
+from constants.types import PAWN, ROOK, KING
 from constants.colors import *
+from util.move import *
 
 pygame.init()
 
@@ -16,14 +17,14 @@ def main():
     text_font = pygame.font.Font(pygame.font.get_default_font(), 20)
 
     board = Board()
-    ia = Ia(board)
+    #ia = Ia(board)
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            ia.move()
+            #ia.move()
             if event.type == pygame.MOUSEBUTTONDOWN:                    
                 if pygame.mouse.get_pos()[1] <= 400:
                     movement_manager(board)
@@ -33,6 +34,7 @@ def main():
         pygame.display.update()
         clock.tick(60)
         board.draw(screen,text_font)
+
 
 
 def movement_manager(board):
@@ -59,15 +61,71 @@ def movement_manager(board):
         # verifica se a casa clicada está realçada
         if selected_house.get_is_highlight():
             
-            candidate_house = selected_house
+            #casa desejada
+            desired_house = selected_house
+            #peca selecionada
+            selected_piece = board.get_selected_piece_house().get_piece()
+            #posicao atual
+            selected_piece_current_pos = board.get_selected_piece_house().get_position()
+            #posicao desejada
+            selected_piece_desired_pos = selected_house.get_position()
 
-            #lógica que registra que já foi dado o primeiro movimento
-            test = board.get_selected_piece_house().get_piece().get_type()
-            if test == PAWN:
-                board.get_selected_piece_house().get_piece().toggle_first_move()
+
+            # en passant
+            if selected_piece.get_type() == PAWN:
+
+                #se o peao estiver fazendo seu segundo movimento,
+                if not selected_piece.get_is_first_move():
+                    #ele nao fica mais vulneravel a captura en passant
+                    selected_piece.set_is_en_passant_vulnerable(False)
+                    
+                selected_piece.toggle_first_move()
+
+                # se o peao fizer o movimento inicial de andar duas casas...
+                if selected_piece.is_special_move(selected_piece_current_pos, selected_piece_desired_pos):
+                    #fica vulneravel ao en passant
+                    selected_piece.set_is_en_passant_vulnerable(True)
+                
+                # se a jogada escolhida for uma captura en passant...
+                if (selected_piece.is_en_passant_capture_move(board, selected_piece_current_pos, selected_piece_desired_pos)):
+                        #se o meu peao for branco,
+                        if selected_piece.get_color() == WHITE:
+                            #o peao adversario capturado esta 'abaixo' do meu
+                            opponent_pawn_house = board.get_house(down(selected_piece_desired_pos))
+
+                        #se o meu peao for preto,
+                        else:
+                            #o peao adversario capturado esta 'acima' do meu
+                            opponent_pawn_house = board.get_house(up(selected_piece_desired_pos))
+
+                        #remove o peao vulneravel
+                        opponent_pawn_house.set_piece(None)
+
+            #roque
+            if selected_piece.get_type() == KING:
+                selected_piece.toggle_first_move()
+
+                #se o movimento escolhido for o roque
+                if selected_piece.is_special_move(selected_piece_current_pos, selected_piece_desired_pos):
+                    
+                    #roque para esquerda
+                    if selected_piece_current_pos[0] > selected_piece_desired_pos[0]:
+                        rook_current_pos = left(left(left(left(selected_piece_current_pos))))
+                        rook_desired_pos = left(selected_piece_current_pos)
+                        
+                    #roque para direita
+                    else:
+                        rook_current_pos = right(right(right(selected_piece_current_pos)))
+                        rook_desired_pos = right(selected_piece_current_pos)
+
+
+                    #movimenta a torre
+                    board.get_house(rook_desired_pos).set_piece(board.get_house(rook_current_pos).get_piece())
+                    board.get_house(rook_current_pos).set_piece(None)
+
 
             # lógica de movimentação e captura
-            candidate_house.set_piece(board.get_selected_piece_house().get_piece())
+            desired_house.set_piece(board.get_selected_piece_house().get_piece())
             board.get_selected_piece_house().set_piece(None)
             board.set_selected_piece_house(None)
             board.switch_turn()
