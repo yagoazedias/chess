a393d8ebbaaaf45752b9da3d0b19e4f7c0b2ddb2
luizlaljr <luizlaljr@gmail.com>
2021-04-21 01:25:56 -0300

    Path changed to pyinstaller

--- constants/images.py
+++ constants/images.py
@@ -1,24 +1,24 @@
 import pygame
 import os
 
-black_pawn = pygame.image.load(os.path.join("./images", "bP.png"))
-black_rook = pygame.image.load(os.path.join("./images", "bR.png"))
-black_horse = pygame.image.load(os.path.join("./images", "bN.png"))
-black_bishop = pygame.image.load(os.path.join("./images", "bB.png"))
-black_queen = pygame.image.load(os.path.join("./images", "bQ.png"))
-black_king = pygame.image.load(os.path.join("./images", "bK.png"))
+black_pawn = pygame.image.load("images/bP.png")
+black_rook = pygame.image.load("images/bR.png")
+black_horse = pygame.image.load("images/bN.png")
+black_bishop = pygame.image.load("images/bB.png")
+black_queen = pygame.image.load("images/bQ.png")
+black_king = pygame.image.load("images/bK.png")
 
-white_pawn = pygame.image.load(os.path.join("./images", "wP.png"))
-white_rook = pygame.image.load(os.path.join("./images", "wR.png"))
-white_horse = pygame.image.load(os.path.join("./images", "wN.png"))
-white_bishop = pygame.image.load(os.path.join("./images", "wB.png"))
-white_queen = pygame.image.load(os.path.join("./images", "wQ.png"))
-white_king = pygame.image.load(os.path.join("./images", "wK.png"))
+white_pawn = pygame.image.load("images/wP.png")
+white_rook = pygame.image.load("images/wR.png")
+white_horse = pygame.image.load("images/wN.png")
+white_bishop = pygame.image.load("images/wB.png")
+white_queen = pygame.image.load("images/wQ.png")
+white_king = pygame.image.load("images/wK.png")
 
-fallen_white_king = pygame.image.load(os.path.join("./images", "fallen_white_king.jpg"))
-fallen_black_king = pygame.image.load(os.path.join("./images", "fallen_black_king.jpg"))
+fallen_white_king = pygame.image.load("images/fallen_white_king.jpg")
+fallen_black_king = pygame.image.load("images/fallen_black_king.jpg")
 
-chess = pygame.image.load(os.path.join("./images", "background.png"))
+chess = pygame.image.load("images/background.png")
 
-credits_screen = pygame.image.load(os.path.join("./images", "credits.png"))
-draw = pygame.image.load(os.path.join("./images", "draw.jpg"))
+credits_screen = pygame.image.load("images/credits.png")
+draw = pygame.image.load("images/draw.jpg")
